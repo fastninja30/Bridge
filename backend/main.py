1d5a0d211015a8cd58365fda938c8e1a6a0dedfd
fastninja30 <spencerrsu@gmail.com>
2025-03-21 16:51:08 -0400

    Removed venv, added directions to start up

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
+
+"""
+1. Cd to backend folder
+2. docker run -p 8000:8000 bridge-backend
+3. http://127.0.0.1:8000/
+4. http://127.0.0.1:8000/docs
+"""
